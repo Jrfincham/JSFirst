800e5014e0cb2d3c98e6a5a99f97b791037b3f4d
Jason Robert Fincham <JRFincham@yahoo.com>
2015-01-14 13:03:39 -0500

    Initial Commit

---
+++ main.js
@@ -0,0 +1,23 @@
+// Declare Vars
+var inputField, button, output, inputValue;
+
+// Set up my vars
+
+inputField = document.getElementById('userText');
+button = document.getElementById('userBtn');
+output = document.getElementById('userName');
+
+
+/*
+1.watch for a click of "button"
+2.when click happens, I grab value of "inputField"
+3.dump that value into my "output" element
+*/
+
+button.onclick = function () {
+
+  inputValue = inputField.value;
+
+  output.inner
+};
+<script type="text/javascript" src="main.js"></script>
